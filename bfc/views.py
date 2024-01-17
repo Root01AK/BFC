f4c0b3627da25282a39d86bf829ea0ac94ce9ac2
Root01AK <Kirubakaran@trdfin.in>
2024-01-18 00:07:55 +0530

    Initial version

---
+++ bfc/views.py
@@ -0,0 +1,20 @@
+from operator import index
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def whychoose(request):
+    return render(request, 'whychoose.html')
+
+def works(request):
+    return render (request, 'works.html')
+
